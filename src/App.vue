d031814aa9a3ed32bc7e8729e3bd1a2506b5880b
Paul <paul@kobusapp.com>
2020-04-07 21:51:08 +0200

    Add vue.js to the project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+        <!-- <div>
+        Score
+        Rules
+
+        You Picked
+        The House Picked
+
+        You Win
+        You Lose
+
+        Play Again
+        </div> -->
+        <Header  score="3" />
+        <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="#">Your Name Here</a>.
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Header
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+        margin: 0;  
+    }
+
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        margin: 0;
+        background: radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+        height: 100vh;
+        color: #2c3e50;
+    }
+
+    .attribution {
+        font-size: 11px;
+        text-align: center;
+        position: absolute;
+        bottom: 5px;
+        left: 0;
+        right: 0;
+    }
+
+    .attribution a {
+        color: hsl(228, 45%, 44%);
+    }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <img
+            id="logo"
+            src="../assets/logo.svg"
+            alt="Logo Rock Paper Scissors"
+        >
+        score {{score}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        props: {
+            score: Number
+        }
+    }
+</script>
+
+<style>
+    #logo {
+        
+    }
+</style>

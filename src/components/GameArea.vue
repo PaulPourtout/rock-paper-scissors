8b0057cc072286d89a09e5683f2a6e67d03c6547
Paul <paul@kobusapp.com>
2020-04-10 23:51:29 +0200

    update animations

--- src/components/GameArea.vue
+++ src/components/GameArea.vue
@@ -20,8 +20,12 @@
                     </div>
             </div>
         </transition>
-        <transition name="slide-fade">
-            <div v-if="step === 2">
+        <transition
+            name="slide-fade"
+            enter-class="slide-fade-enter"
+            leave-to-class="fade-out"
+        >
+            <div v-if="step === 2" class="step-2-container">
                 <div class="row">
                     <div class="pick-container">
                         <ButtonGame
@@ -31,21 +35,25 @@
                         <p class="pick-label">You picked</p>
                     </div>
                     <div class="pick-container">
-                        <ButtonGame
-                            v-bind:type="computerChoice"
-                            v-bind:disabled="true"
-                        />
+                            <ButtonGame
+                                v-bind:type="computerChoice"
+                                v-bind:disabled="true"
+                            />
                         <p class="pick-label">The house picked</p>
                     </div>
                 </div>
-                <div v-if="winner" class="result-part">
-                    <p>{{winner === "player" ? 'You win' : winner === "even" ? "Equality !" : 'You lose'}}</p>
-                    <Button
-                        label="Play again"
-                        variant="primary"
-                        v-bind:handle-click="handleNewGame"
-                    />
-                </div>
+                <transition
+                    name="slide-to-top"
+                >
+                    <div v-if="winner" class="result-part">
+                        <p>{{winner === "player" ? 'You win' : winner === "even" ? "Equality !" : 'You lose'}}</p>
+                        <Button
+                            label="Play again"
+                            variant="primary"
+                            v-bind:handle-click="handleNewGame"
+                        />
+                    </div>
+                </transition>
             </div>
         </transition>
 
@@ -95,7 +103,8 @@
             },
             handleChoseAction: function (choice) {
                 this.playerChoice = choice;
-                setTimeout(this.generateComputerAnswer, 1000);
+                setTimeout(() => this.step = 2, 550);
+                setTimeout(this.generateComputerAnswer, 2000);
             },
             generateComputerAnswer: function() {
                 const possibleResults = ["rock", "paper", "scissors"];
@@ -103,7 +112,6 @@
                 this.computerChoice = possibleResults[resultIndex];
 
                 setTimeout(() => this.checkPlayerVictory(this.playerChoice, this.computerChoice), 700);
-                this.step = 2;
             },
             handleNewGame: function () {
                 this.step = 1;
@@ -141,6 +149,12 @@
         justify-content: space-around;
     }
 
+    .step-2-container {
+        display: flex;
+        flex-direction: column;
+        height: 17rem;
+    }
+
     .row {
         display: flex;
         justify-content: space-around;
@@ -175,10 +189,29 @@
         transition: all .3s ease;
     }
     .slide-fade-leave-active {
-        transition: all .7s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+        transition: all .5s cubic-bezier(1.0, 0.5, 0.8, 1.0);
     }
-    .slide-fade-enter, .slide-fade-leave-to {
+    
+    .slide-fade-enter {
+        transform: translateX(-10px);
+        opacity: 0;
+    }
+    
+    .slide-fade-leave-to {
         transform: translateX(10px);
+        opacity: 0;
+    }
+
+    .slide-to-top-enter {
+        transform: scale(1.5);
+        z-index: 10;
+        opacity: 0;
+    }
+    .slide-to-top-enter-active {
+        transition: all .15s ease-in;
+    }
+
+    .fade-out {
         opacity: 0;
     }
 
